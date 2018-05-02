73d207e294bedc0bbc47413d37598ea7c7396562
huong <huongnguyenak96@gmail.com>
2018-05-02 11:31:31 +0700

    collect

--- routes/web.php
+++ routes/web.php
@@ -50,3 +50,28 @@ Route::get('/logoutOthers', function(){
     auth()->logoutOtherDevices('password'); //logoutOtherDevices is a function be added in SessionGuard class 
     return redirect('/');
 });
+
+
+Route::get('collect',function(){
+    $items = [
+        ['name' => 'David Charleston', 'member' => 1, 'active' => 1],
+        ['name' => 'Blain Charleston', 'member' => 0, 'active' => 0],
+        ['name' => 'Megan Tarash', 'member' => 1, 'active' => 1],
+        ['name' => 'Jonathan Phaedrus', 'member' => 1, 'active' => 1],
+        ['name' => 'Paul Jackson', 'member' => 0, 'active' => 1]
+    ];
+    // return collect($items)
+    //         ->where('active', 1)
+    //         ->tap(function($collection){
+    //             return var_dump($collection->pluck('name'));
+    //         })
+    //         ->where('member', 1)
+    //         ->tap(function($collection){
+    //             return var_dump($collection->pluck('name'));
+    //         });    
+    return collect($items)
+            ->where('active', 1)
+            ->pipe(function ($collection) {
+                return $collection->push(['name' => 'John Doe']);
+            });
+});
